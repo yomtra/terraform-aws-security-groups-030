69200b050f166fd3da734b7d501249552780146d
yomtra <bugarinartyom2@gmail.com>
2025-04-25 20:13:09 -0500

    added main.tf

---
+++ main.tf
@@ -0,0 +1,14 @@
+variable "vpc_id" {
+    type = string
+}
+
+resource "aws_security_group" "default" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = var.vpc_id
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
